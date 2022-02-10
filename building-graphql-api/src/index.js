51844fed4b26248ec3fb2f09951b8e5182b57fe3
unknown <dwilliams_64@yahoo.com>
2022-02-10 17:54:54 -0600

    Setup graphql express server, setup basic session schema for graphql

---
+++ building-graphql-api/src/index.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const express = require("express");
+const { loadFilesSync } = require("@graphql-tools/load-files");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
+const { ApolloServer } = require("apollo-server-express");
+
+const typesArray = loadFilesSync(path.join(__dirname, "**/*.graphql"));
+
+// const resovlersArray = loadFilesSync(path.join(__dirname, "**/*.resolvers.js"));
+
+const startApolloServer = async () => {
+  const app = express();
+
+  const PORT = 4000;
+
+  //   resolvers: resovlersArray,
+
+  const schema = makeExecutableSchema({
+    typeDefs: typesArray,
+  });
+
+  const server = new ApolloServer({
+    schema,
+  });
+
+  await server.start();
+
+  server.applyMiddleware({ app, path: "/graphql" });
+
+  app.listen(PORT, () => console.log(`Graphql on port ${PORT}`));
+};
+
+startApolloServer();
