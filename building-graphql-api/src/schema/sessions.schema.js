87e2162c29533a04fde6fb3717e5307c69c2ab97
unknown <dwilliams_64@yahoo.com>
2022-02-28 18:59:20 -0600

    modularized schema

---
+++ building-graphql-api/src/schema/sessions.schema.js
@@ -0,0 +1,32 @@
+const { gql } = require("apollo-server-express");
+
+module.exports = gql`
+  type Query {
+    sessions(
+      id: ID
+      title: String
+      description: String
+      startsAt: String
+      endsAt: String
+      room: String
+      day: String
+      format: String
+      track: String
+      level: String
+    ): [Session]
+    session(id: ID!): Session
+  }
+
+  type Session {
+    id: ID!
+    title: String!
+    description: String
+    startsAt: String
+    endsAt: String
+    room: String
+    day: String
+    format: String
+    track: String @deprecated(reason: "Moving to a better model.")
+    level: String
+  }
+`;
