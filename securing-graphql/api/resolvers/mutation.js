113747f84d4a1c40c4cef4944098c352ef57f08e
unknown <dwilliams_64@yahoo.com>
2023-03-05 15:58:22 -0600

    securing-graphql: Created signIn resolver to handle user sign in.

--- securing-graphql/api/resolvers/mutation.js
+++ securing-graphql/api/resolvers/mutation.js
@@ -13,42 +13,53 @@ module.exports = {
     input.email = input.email.toLowerCase();
     const { email, password } = input;
 
-    // Check to see if user is in DB
     const doesUserExist = await userDataSource.getUserByEmail(email);
 
-    console.log(doesUserExist);
-
-    console.log(doesUserExist);
-    // If in DB return apollo error
     if (doesUserExist) {
       throw new Error("User already exists");
     }
 
-    // If user is found hash password
-
     const hashedPassword = authUtils.hashPassword(password);
-
-    // store user creds in db
 
     const newUser = await userDataSource.createUser({
       email,
       password: hashedPassword,
     });
 
-    // create token
-
     const token = authUtils.createToken({
       id: newUser.id,
       email: newUser.email,
     });
 
-    // Return auth token
-
-    console.log(newUser);
-
-    return { token, user: { id: newUser.id, email: newUser.email } };
+    return { token, user: { ...newUser } };
   },
+  signIn: async (parent, { input }, { dataSources }, info) => {
+    const { userDataSource } = dataSources;
 
+    input.email = input.email.toLowerCase();
+
+    const { email, password } = input;
+
+    const user = userDataSource.getUserByEmail(email);
+
+    if (!user) {
+      throw new Error("Account cannot be found.  Please create an account.");
+    }
+
+    const isValidPassword = authUtils.verifyPassword(password, user.password);
+
+    if (!isValidPassword) {
+      throw new Error("Invalid password.");
+    }
+    const token = authUtils.createToken({
+      ...user,
+    });
+
+    return {
+      token,
+      user: { ...user },
+    };
+  },
   toggleFavoriteSession: async (parent, args, { dataSources }, info) => {
     const speaker = await dataSources.sessionDataSource.toggleFavoriteSession(
       args.id
